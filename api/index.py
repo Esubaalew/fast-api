6954487705f359d793c0b3aeeb9dfad9b6422868
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-30 13:57:48 +0300

    add diction package, new endpints etc

--- api/index.py
+++ api/index.py
@@ -4,41 +4,24 @@ import requests
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
+
+from dictionary.britannica import (
+    get_entries,
+    get_total_entries,
+    get_word_of_the_day,
+    get_parts,
+    get_definitions
+)
+
 TOKEN = os.environ.get("TOKEN")
 TELEGRAM_API_BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 
 app = FastAPI()
 
-class TelegramWebhook(BaseModel):
-    update_id: int
-    message: Optional[dict]
-    edited_message: Optional[dict]
-    channel_post: Optional[dict]
-    edited_channel_post: Optional[dict]
-    inline_query: Optional[dict]
-    chosen_inline_result: Optional[dict]
-    callback_query: Optional[dict]
-    shipping_query: Optional[dict]
-    pre_checkout_query: Optional[dict]
-    poll: Optional[dict]
-    poll_answer: Optional[dict]
-
-def start(chat_id):
-    # Send a message using the plain Telegram Bot API
-    endpoint = f"{TELEGRAM_API_BASE_URL}/sendMessage"
-    params = {
-        "chat_id": chat_id,
-        "text": "I'm a bot, please talk to me!"
-    }
-    requests.post(endpoint, json=params)
-
-@app.post("/webhook")
-def webhook(webhook_data: TelegramWebhook):
-    if webhook_data.message and webhook_data.message.text == '/start':
-        # Handle the '/start' command
-        start(webhook_data.message.chat.id)
-
-    return {"message": "ok"}
+@app.get("/britannica/entries/{word}")
+async def britannica_entries(word: str):
+    entries = get_entries(word)
+    return {"entries": entries}
 
 @app.get("/")
 def index():
